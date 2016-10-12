9e4e9985d0f2b4d549354cd2aabc90024387f05d
jasmi <jasmi@DESKTOP-T3PI036>
2016-10-12 15:40:19 -0400

    1st part

---
+++ File IO/File IO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace jwhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fileName = "C:\\Users\\jasmi\\Documents\\first.txt";
+            //List<Numbers> numbers = new List<Numbers>();
+
+            Numbers n = new Numbers();
+            n.MyNumber = "The number is {0}";
+
+            StreamWriter sw = new StreamWriter(fileName, false);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Numbers n1 = new Numbers();
+                sw.WriteLine(string.Format("{0}", i + 1));
+            }
+            sw.Close();
+
+            StreamReader sr = new StreamReader(fileName);
+            List<Numbers> numb = new List<Numbers>();
+            if (File.Exists(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string temp = sr.ReadLine();
+                    string[] value = temp.Split('\n');
+
+                    Numbers n2 = new Numbers();
+                    n2.MyNumber = value[0];
+
+                    numb.Add(n2);
+                }
+            }
+            foreach (Numbers n3 in numb)
+            {
+                Console.WriteLine("{0}", n3.MyNumber);
+            }
+           
+            Console.ReadKey();
+        }
+
+    }
+}
